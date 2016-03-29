a0731d3690fd4ae59afcc8ce3f0e3a7ef6624edf
arinirohmawati6706144105 <superarin182@gmail.com>
2016-03-29 15:15:25 +0700

    Added files via upload

---
+++ fibonacci.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fibonacci;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Praktikum
+ */
+
+public class fibonacci
+{
+ public static void main(String[]args)
+ {
+  int a = Integer.parseInt(JOptionPane.showInputDialog("Masukkan nilai awal Deret Fibonacci"));
+  int b = a;
+  int c = Integer.parseInt(JOptionPane.showInputDialog("Masukkan deret Fibonacci yang Anda Inginkan: "));
+  int d = c-1;
+  int e = 1;
+  System.out.print(a + " ");
+  while(e <=d)
+  {
+   System.out.print(a +" ");
+   a = a+b;
+   b = a-b;
+   e++;
+  }
+ }
+}
